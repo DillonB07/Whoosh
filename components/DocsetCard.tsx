f31e2ab975986d3c19258a987d94b30a100007dd
DillonB07 <dillonb07dev@gmail.com>
2023-05-12 17:35:54 +0100

    Revert "Revert "Start docs page""
    This reverts commit ffae6d3a0ec0e48f46bc865d878007031913e861.

---
+++ components/DocsetCard.tsx
@@ -0,0 +1,66 @@
+import { Link } from "icons";
+import { Pill, Surface, Text, Tooltip, View, interactive, rcss, tokens } from "rui";
+import { docsetItemType } from "utils/getDocs";
+import * as icons from '../icons';
+
+const DocsetCard: React.FC<docsetItemType> = ({
+    name,
+    version,
+    downloaded,
+}) => (
+    <Surface
+        elevated
+        css={[
+            rcss.p(16),
+            rcss.m(8),
+            rcss.borderRadius(8),
+            interactive.filledAndOutlined,
+            rcss.maxWidth(210),
+            rcss.maxHeight(104),
+        ]}
+    >
+        <Link href={`/docs/${name}`} css={[{ color: tokens.foregroundDefault }]}>
+            <View css={[rcss.rowWithGap(16), rcss.center]}>
+                {downloaded ? (
+                    <icons.BookClosed width={50} height={50} />
+                ) : (
+                    <icons.Download width={50} height={50} />
+                )}
+
+                <View css={[rcss.colWithGap(4), rcss.center]}>
+                    <Tooltip tooltip={name.replace(/_/g, " ")}>
+                        <Text
+                            variant="subheadBig"
+                            css={[
+                                {
+                                    overflow: "hidden",
+                                    textOverflow: "ellipsis",
+                                    maxWidth: "100px",
+                                },
+                            ]}
+                        >
+                            {name.replace(/_/g, " ")}
+                        </Text>
+                    </Tooltip>
+                    <Text variant="small" color="dimmer">
+                        v{version}
+                    </Text>
+                    <Tooltip
+                        tooltip={
+                            downloaded
+                                ? "This docset is downloaded"
+                                : "This docset needs downloading and could take a while to load"
+                        }
+                    >
+                        <Pill
+                            css={[rcss.width(110)]}
+                            text={downloaded ? "Downloaded" : "Not Downloaded"}
+                            colorway={downloaded ? "positive" : "negative"}
+                        />
+                    </Tooltip>
+                </View>
+            </View>
+        </Link>
+    </Surface>
+);
+export default DocsetCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,83 +2,17 @@ import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
-  Button,
   rcss,
   Surface,
-  tokens,
-  interactive,
-  Pill,
-  Tooltip,
   SearchBar,
 } from "../rui";
 import { getPackages } from "../utils/getDocs";
 import type { docsetItemType } from "../utils/getDocs";
 import * as icons from "../icons";
-import Link from "next/link";
 import Fuse from "fuse.js";
 import { motion } from "framer-motion";
 import LoadingIcon from "icons/Loading";
-
-const DocsetCard: React.FC<docsetItemType> = ({
-  name,
-  version,
-  downloaded,
-}) => (
-  <Surface
-    elevated
-    css={[
-      rcss.p(16),
-      rcss.m(8),
-      rcss.borderRadius(8),
-      interactive.filledAndOutlined,
-      rcss.maxWidth(210),
-      rcss.maxHeight(104),
-    ]}
-  >
-    <Link href={`/docs/${name}`} css={[{ color: tokens.foregroundDefault }]}>
-      <View css={[rcss.rowWithGap(16), rcss.center]}>
-        {downloaded ? (
-          <icons.BookClosed width={50} height={50} />
-        ) : (
-          <icons.Download width={50} height={50} />
-        )}
-
-        <View css={[rcss.colWithGap(4), rcss.center]}>
-          <Tooltip tooltip={name.replace(/_/g, " ")}>
-            <Text
-              variant="subheadBig"
-              css={[
-                {
-                  overflow: "hidden",
-                  textOverflow: "ellipsis",
-                  maxWidth: "100px",
-                },
-              ]}
-            >
-              {name.replace(/_/g, " ")}
-            </Text>
-          </Tooltip>
-          <Text variant="small" color="dimmer">
-            v{version}
-          </Text>
-          <Tooltip
-            tooltip={
-              downloaded
-                ? "This docset is downloaded"
-                : "This docset needs downloading and could take a while to load"
-            }
-          >
-            <Pill
-              css={[rcss.width(110)]}
-              text={downloaded ? "Downloaded" : "Not Downloaded"}
-              colorway={downloaded ? "positive" : "negative"}
-            />
-          </Tooltip>
-        </View>
-      </View>
-    </Link>
-  </Surface>
-);
+import DocsetCard from "components/DocsetCard";
 
 const Home: React.FC = () => {
   const [pkgsLoaded, setPkgsLoaded] = useState(false);
@@ -159,29 +93,25 @@ const Home: React.FC = () => {
             }}
             layout
           >
-            {pkgsLoaded && (
-              <>
-                {filteredPackages.map((pkg, i) => (
-                  <DocsetCard
-                    key={i}
-                    name={pkg.name}
-                    version={pkg.version}
-                    downloaded={pkg.downloaded}
-                  />
-                ))}
-              </>
-            )}
+            {pkgsLoaded && filteredPackages.map((pkg, i) => (
+              <DocsetCard
+                key={i}
+                name={pkg.name}
+                version={pkg.version}
+                downloaded={pkg.downloaded}
+              />
+            ))}
           </motion.div>
         </Surface>
         {!pkgsLoaded && (
           <motion.div css={[rcss.flex.column, rcss.center, { gap: 8 }]}>
             {!loadFailed ? <LoadingIcon width={50} height={50} /> : <icons.WifiOff width={50} height={50} />}
 
-              {!loadFailed ?  (<Text variant="subheadBig" color="dimmer" multiline>Loading package list</Text>) : (
-                <Text variant="text" color="dimmer" multiline>Could not connect to server.<br/>
+            {!loadFailed ? (<Text variant="subheadBig" color="dimmer" multiline>Loading package list</Text>) : (
+              <Text variant="text" color="dimmer" multiline>Could not connect to server.<br />
                 Make sure *.repl.co domains are not blocked on your network
-                </Text>
-                )}
+              </Text>
+            )}
           </motion.div>
         )}
       </motion.div>
